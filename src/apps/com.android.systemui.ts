247fd5a2ce88c8d84df1caaf309a7f01fbbeb957
windrua <34590060+sfwtw@users.noreply.github.com>
2024-10-12 22:20:37 +0800

    Update com.android.systemui.ts

--- src/apps/com.android.systemui.ts
+++ src/apps/com.android.systemui.ts
@@ -1,22 +1,21 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
 export default defineGkdApp({
-  id: 'com.android.systemui',
+  id: com.android.systemui',
   name: '系统 UI',
   groups: [
     {
       key: 1,
-      name: '功能-自动允许调试',
+      name: '权限提示-通知权限',
+      desc: '点击[取消]',
+      fastQuery: true,
+      matchTime: 10000,
       rules: [
         {
-          key: 0,
-          name: '播放列表-右侧悬浮广告',
-          fastQuery: true,
-          activityIds:
-            'null',
           matches: '[text="允许 USB 调试吗？"] <<2 LinearLayout +3 [id="android:id/buttonPanel"] >2 Button[text="允许"]',
-          snapshotUrls: 'https://i.gkd.li/i/13348489',
+          snapshotUrls: 'https://i.gkd.li/i/15796616',
         },
       ],
     },
+  ],
 });
